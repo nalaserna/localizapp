7fc1d910acf489ff03a742ad606f2477f24af4f0
Natalia Laserna <nalaserna@poligran.edu.co>
2020-04-11 10:53:51 -0500

    Maven installed and working!

--- localizapp/localizapp/src/main/java/com/localizapp/localizapp/LocalizappApplication.java
+++ localizapp/localizapp/src/main/java/com/localizapp/localizapp/LocalizappApplication.java
@@ -1,19 +1,17 @@
-package com.localizapp;
+package com.localizapp.localizapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-@ComponentScan
-(basePackages="com.localizapp")
-@EntityScan(basePackages= {"entities"})
-@EnableJpaRepositories("repositories")
+@ComponentScan (basePackages="com.localizapp,servicios,repositories,entities")
+@EntityScan(basePackages = {"entities"}) 
+@EnableJpaRepositories ("repositories")
 public class LocalizappApplication {
 
 	public static void main(String[] args) {
---
+++ localizapp/localizapp/src/test/java/com/localizapp/localizapp/LocalizappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.localizapp.localizapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LocalizappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
