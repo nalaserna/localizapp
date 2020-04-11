00f95d2498ff2be55d65b3365e7c700de2f5027f
Natalia Laserna <nalaserna@poligran.edu.co>
2020-04-11 14:24:12 -0500

    Evento repository created

---
+++ localizapp/localizapp/src/main/java/repositories/EventoRepository.java
@@ -0,0 +1,29 @@
+package repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
+
+import entities.Evento;
+import entities.Usuario;
+
+@Repository
+public interface EventoRepository extends CrudRepository<Evento, Long> {
+	
+	public Evento findById(int id);
+	
+	public List<Evento> findByNombre(String nombre);
+	
+	public List<Evento> findByFechaInicio(Date fechaInicio);
+	
+	public List<Evento> findByFechaFin(Date fechaFin);
+	
+	public List<Evento> findByHoraInicio(String horaInicio);
+	
+	public List<Evento> findByHoraFin(String horaFin);
+	
+	public Usuario findByCorreo(String correo);
+	
+}
