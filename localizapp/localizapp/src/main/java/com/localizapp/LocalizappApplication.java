f457e826d74fc2d5386675f53ef00506db9b3359
unknown <nalaserna@poligran.edu.co>
2020-04-10 22:30:27 -0500

    Maven project created

---
+++ localizapp/localizapp/src/main/java/com/localizapp/LocalizappApplication.java
@@ -0,0 +1,13 @@
+package com.localizapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalizappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalizappApplication.class, args);
+	}
+
+}
---
+++ localizapp/localizapp/src/test/java/com/localizapp/LocalizappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.localizapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalizappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
