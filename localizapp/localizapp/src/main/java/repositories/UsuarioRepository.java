e85c6c518958b6fe9b96d82cd1a8c669c5f8e680
Natalia Laserna <nalaserna@poligran.edu.co>
2020-04-11 13:25:50 -0500

    Created Usuario repository

---
+++ localizapp/localizapp/src/main/java/repositories/UsuarioRepository.java
@@ -0,0 +1,18 @@
+package repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import entities.Usuario;
+
+@Repository
+public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
+	
+	public Usuario findById(int id);
+	
+	public List<Usuario> findByEsAdmin(Byte esAdmin);
+	
+	public Usuario findByCorreo(String correo);
+	
+}
