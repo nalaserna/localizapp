d044616b46e86837e766e4ff542dfa06b573f469
Natalia Laserna <nalaserna@poligran.edu.co>
2020-04-11 22:14:42 -0500

    Página principal creada, mapa mostrándose

--- localizapp_front/src/app/home/home.module.ts
+++ localizapp_front/src/app/home/home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
@@ -11,6 +12,7 @@ import { HomePage } from './home.page';
     CommonModule,
     FormsModule,
     IonicModule,
+    HttpModule,
     RouterModule.forChild([
       {
         path: '',
--- localizapp_front/src/app/home/home.page.ts
+++ localizapp_front/src/app/home/home.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { Map, tileLayer, marker, icon } from 'leaflet';
+import { Http } from '@angular/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +11,21 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(
+              public plt: Platform,
+              public router: Router) {
+                
+              }
 
-  constructor() {}
-
+ngOnInit(){
+  this.initMap();
 }
+  initMap() {
+    const map = new Map('map').setView([4.730107214, -74.03782700], 20);           
+    tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map);
+              
+    }
+}
+
