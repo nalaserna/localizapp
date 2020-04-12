7054a09284d5d15517b0613708f517af0f4c6e4f
Natalia Laserna <nalaserna@poligran.edu.co>
2020-04-11 23:56:39 -0500

    Leaflet pin showing

--- localizapp_front/src/app/home/home.page.ts
+++ localizapp_front/src/app/home/home.page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { Router } from '@angular/router';
-import { Map, tileLayer, marker, icon } from 'leaflet';
+import { Map, tileLayer, marker, icon, L} from 'leaflet';
 import { Http } from '@angular/http';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { map } from 'rxjs/operators';
@@ -14,6 +14,7 @@ import { map } from 'rxjs/operators';
 export class HomePage {
   latitud : number;
   longitud : number;
+  
   constructor(
               private geolocation: Geolocation,
               public plt: Platform,
@@ -22,22 +23,35 @@ export class HomePage {
               }
 
 ngOnInit(){
+  
+
   this.geolocation.getCurrentPosition().then((resp) => {
     this.latitud = resp.coords.latitude;
     this.longitud = resp.coords.longitude;
     this.initMap();
+    
+  //  .on('click',()=>this.router.navigateByUrl('/agregarPunto'))
+  //  .addTo(map).openPopup();
    }).catch((error) => {
      console.log('Error getting location', error);
    });
   
 }
   initMap() {
+    const customMarkerIcon = icon({
+      iconUrl: 'assets/images/nati.png',
+      iconSize: [50, 60], 
+      popupAnchor: [0, -20]
+    });
+
     const map = new Map('map').setView([this.latitud, this.longitud], 20);           
     tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       }).addTo(map);
-              
+      marker([this.latitud, this.longitud], {icon: customMarkerIcon})
+      .bindPopup(`Muéveme y haz click para agregar un punto`, { autoClose: false }).addTo(map).openPopup();        
     }
+
 
     
      
