e1fa2848d4c0cae2ba4c07195ff6c3d66248479a
Natalia Laserna <nalaserna@poligran.edu.co>
2020-04-11 23:07:55 -0500

    Reconoce lat/lon wuju

--- localizapp_front/src/app/home/home.page.ts
+++ localizapp_front/src/app/home/home.page.ts
@@ -3,6 +3,7 @@ import { Platform } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { Map, tileLayer, marker, icon } from 'leaflet';
 import { Http } from '@angular/http';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { map } from 'rxjs/operators';
 
 @Component({
@@ -11,21 +12,42 @@ import { map } from 'rxjs/operators';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  latitud : number;
+  longitud : number;
   constructor(
+              private geolocation: Geolocation,
               public plt: Platform,
               public router: Router) {
                 
               }
 
 ngOnInit(){
-  this.initMap();
+  this.geolocation.getCurrentPosition().then((resp) => {
+    this.latitud = resp.coords.latitude;
+    this.longitud = resp.coords.longitude;
+    this.initMap();
+   }).catch((error) => {
+     console.log('Error getting location', error);
+   });
+  
 }
   initMap() {
-    const map = new Map('map').setView([4.730107, -74.037827], 20);           
+    const map = new Map('map').setView([this.latitud, this.longitud], 20);           
     tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       }).addTo(map);
               
     }
+
+    
+     
+    // let watch = this.geolocation.watchPosition();
+    // watch.subscribe((data) => {
+      // data can be a set of coordinates, or an error (if an error occurred).
+      // data.coords.latitude
+      // data.coords.longitude
+    // });
+
+    
 }
 
